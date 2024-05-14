c012dc20f3f7752269c13658bb81c6dea79cbc02
jdbenitop <167857170+jdbenitop@users.noreply.github.com>
2024-05-14 11:59:11 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,48 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { CardStyleInterpolators } from '@react-navigation/stack';
+import MisionCumplida from './Screens/MisionCumplida';
 
-// Importación de componentes de pantalla
-import Portada from './Screens/Portada';
-import Opcions from './Screens/Opcions';
-import CrearScreen from './Screens/CrearScreen';
-import Partides from './Screens/Partides';
-
-
-// Creación de un stack navigator
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Portada">
-        <Stack.Screen name="Portada" component={Portada} options={{ headerShown: false }} />
-        <Stack.Screen
-          name="Opcions"
-          component={Opcions}
-          options={{
-            presentation: 'transparentModal', 
-            cardStyleInterpolator: CardStyleInterpolators.forUIKit, 
-            headerShown: false,
-          }}
-        />
-         <Stack.Screen
-          name="Partides"
-          component={Partides}
-          options={{
-            headerShown: false,
-            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-          }}
-        />
-        <Stack.Screen
-          name="CrearScreen"
-          component={CrearScreen}
-          options={{
-            headerShown: false,
-            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-          }}
-        />
+      <Stack.Navigator initialRouteName="MisionCumplida">
+        <Stack.Screen name="MisionCumplida" component={MisionCumplida} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
