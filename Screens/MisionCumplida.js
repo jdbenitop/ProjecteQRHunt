a2f12ff52c060bdc30c5b53b927f2167302ee0a6
jdbenitop <167857170+jdbenitop@users.noreply.github.com>
2024-05-21 12:53:26 +0200

    Update MisionCumplida.js

--- Screens/MisionCumplida.js
+++ Screens/MisionCumplida.js
@@ -1,43 +1,51 @@
 import React, { useRef } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Animated, ImageBackground} from 'react-native';
 
 export default function PantallaPrincipal({ navigation }) {
-  const animatedValue = useRef(new Animated.Value(0)).current; // Usamos useRef para crear una referencia mutable
+  const animatedValue = useRef(new Animated.Value(0)).current;
 
   const handlePress = () => {
     navigation.navigate('MisionCumplida');
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>MISSION COMPLETADA</Text>
-     
-      <Text style={styles.subtitle}>Muy bien aventurero, encontraste el tesoro escondido!</Text>
-      <Animated.Image
-        source={require('../Images/Extra_Image.png')} 
-        style={[styles.image, { transform: [{ rotateY: animatedValue.interpolate({ inputRange: [0, 1], outputRange: ['0deg', '180deg'] }) }] }]}
-      />
-      <TouchableOpacity style={styles.button} onPress={() => {
-        Animated.timing(animatedValue, {
-          toValue: 1,
-          duration: 1000, // Duración de la animación en milisegundos
-          useNativeDriver: true,
-        }).start(() => {
-          handlePress();
-        });
-      }}>
-        <Text style={styles.buttonText}>CONTINUAR</Text>
-      </TouchableOpacity>
-    </View>
+    <ImageBackground
+      source={require('../Images/Home_Background_Animation00.png')} // Ajusta la ruta según la estructura de directorios
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>MISSION COMPLETADA</Text>
+        <Text style={styles.subtitle}>Muy bien aventurero, encontraste el tesoro escondido!</Text>
+        <Animated.Image
+          source={require('../Images/Extra_Image.png')} 
+          style={[styles.image, { transform: [{ rotateY: animatedValue.interpolate({ inputRange: [0, 1], outputRange: ['0deg', '180deg'] }) }] }]}
+        />
+        <TouchableOpacity style={styles.button} onPress={() => {
+          Animated.timing(animatedValue, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: true,
+          }).start(() => {
+            handlePress();
+          });
+        }}>
+          <Text style={styles.buttonText}>CONTINUAR</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover', // o 'stretch' si quieres que la imagen se estire para cubrir todo el contenedor
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Color de fondo con transparencia
   },
   title: {
     fontSize: 45,
@@ -61,7 +69,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   image: {
-    width: 200, 
+    width: 200,
     height: 200,
     marginBottom: 20,
   },
